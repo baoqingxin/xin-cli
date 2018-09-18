1b5a5b30fa750c79cf08e1b64761a444cee36ab9
baoqingxin <1142634487@qq.com>
2018-09-18 10:51:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# 前言
+一个简单vue脚手架工具  
+仅供参考  
+## 使用
+全局安装
+> npm install -g xin-cli1737  
+
+新建xxx项目
+> xin1737 init xxx
+
+> cd xxx
+
+> npm install
+
+## 说明
+这是一个简单的vue脚手架，待完善
---
+++ bin/xin-cli.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+const fs = require('fs')
+const inquirer = require('inquirer')
+const program = require('commander');
+const download = require('download-git-repo');
+const chalk = require('chalk');
+const ora = require('ora');
+const symbols = require('log-symbols');
+const handlebars = require('handlebars');
+
+program.version('0.0.1', '-v', '--version')
+        .command('init <name>')
+        .action((name) => {
+          // console.log(name)
+          if (!fs.existsSync(name)) {
+            inquirer.prompt([
+              {
+                name: 'description',
+                message: '请输入项目描述'
+              },
+              {
+                name: 'author',
+                message: '请输入作者名称'
+              }
+            ]).then((answers) => {
+              console.log(answers.author);
+              const spinner = ora('正在下载模板...')
+              spinner.start()
+              download('direct:https://github.com/baoqingxin/webpack-vue.git#master', name, {clone: true}, (err) => {
+                if (err) {
+                  spinner.fail()
+                  console.log(symbols.error, chalk.red(err))
+                } else {
+                  spinner.succeed()
+                  const fileName = `${name}/package.json`
+                  const meta = {
+                    name,
+                    description: answers.description,
+                    author: answers.author
+                  }
+
+                  if (fs.existsSync(fileName)) {
+                    const content = fs.readFileSync(fileName).toString()
+                    const result = handlebars.compile(content)(meta)
+                    fs.writeFileSync(fileName, result)
+                  }
+                  console.log(symbols.success, chalk.green('项目初始化完成'))
+                }
+              })
+            })
+          } else {
+            console.log(symbols.error, chalk.red('项目已存在'))
+          }
+          // download('github:baoqingxin/webpack-vue', 'master', {clone: true}, (err) => {
+        })
+program.parse(process.argv)
